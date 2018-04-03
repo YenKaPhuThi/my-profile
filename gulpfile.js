a5a7daad0131723fdf8deca8715721ac15471814
thi-yen.ka-phu <thi-yen.ka-phu@ekino.com>
2018-04-03 18:20:55 +0700

    fix(TR|Front): #nav# start with menu

--- gulpfile.js
+++ gulpfile.js
@@ -7,27 +7,27 @@ gulp.task('index', function() {
   return gulp.src('app/*.pug')
   .pipe(pug())
   .pipe(gulp.dest('build'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+  .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('pug', function(){
   return gulp.src('app/tmpl/**/*.pug')
   .pipe(pug())
   .pipe(gulp.dest('build/tmpl'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+  .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('sass', function() {
   return gulp.src('app/assets/scss/*.scss')
   .pipe(sass())
   .pipe(gulp.dest('build/assets/css'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+  .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function() {
+  return gulp.src('app/assets/js/*.js')
+  .pipe(gulp.dest('build/assets/js'))
+  .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('browserSync', function() {
@@ -38,10 +38,11 @@ gulp.task('browserSync', function() {
   })
 });
 
-gulp.task('watch', ['index', 'pug', 'sass', 'browserSync'], function(){
+gulp.task('watch', ['index', 'pug', 'sass', 'js', 'browserSync'], function(){
   gulp.watch('app/*.pug', browserSync.reload);
   gulp.watch('app/tmpl/**/*.pug', browserSync.reload);
   gulp.watch('app/assets/scss/*.scss', ['sass']);
-  gulp.watch('app/assets/js/*.js', browserSync.reload);
+  gulp.watch('app/assets/scss/**/*.scss', ['sass']);
+  gulp.watch('app/assets/js/*.js', ['js']);
 });
 
